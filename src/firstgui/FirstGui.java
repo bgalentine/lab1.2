e760d8eab13d01f7eeedc063212ff5d0e2965387
Administrator <administrator@edsmacbook-pro.gateway.pace.com>
2014-02-02 01:15:11 -0800

    fixed FirstGui

--- src/firstgui/FirstGui.java
+++ src/firstgui/FirstGui.java
@@ -14,50 +14,70 @@ package firstgui;
  *
  * @author Byron Galentine
  */
-
+//import statements for swing & awt
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FirstGui {
+
+public class FirstGui extends JFrame {
+    JButton northWestButton, southWestButton, northEastButton, southEastButton;
+    
+    public FirstGui(String name){
+        super(name);
+        setResizable(false);
+    }
+    
     public final static boolean RIGHT_TO_LEFT = false;
 
-    public static void addComponentsToPane(Container contentPane) {
-        if (RIGHT_TO_LEFT) {
+    public void createContentPane(Container contentPane) {
+       if (RIGHT_TO_LEFT) {
             contentPane.setComponentOrientation(
                 ComponentOrientation.RIGHT_TO_LEFT);
         }
-//        Any number of rows and 3 columns
-        contentPane.setLayout(new GridLayout(0,3));
+//        Any number of rows and 2 columns
+        contentPane.setLayout(new GridLayout(0,2));
 
-        //Add 9 JButtons for all the quadrants
-        contentPane.add(new JButton("North West Quadrant"));
-        contentPane.add(new JButton("West Central Quadrant"));
-        contentPane.add(new JButton("South West Quadrant"));
-        contentPane.add(new JButton("North Central Quadrant"));
-        contentPane.add(new JButton("Central Quadrant"));
-        contentPane.add(new JButton("South Central Quadrant"));
-        contentPane.add(new JButton("North East Quadrant"));
-        contentPane.add(new JButton("East Central Quadrant"));
-        contentPane.add(new JButton("South East Quadrant"));
+        //Add 4 JButtons for first four quadrants
+        northWestButton = new JButton("North West");
+        contentPane.add(northWestButton);
+        //contentPane.add(new JButton("North Central Quadrant"));
+        
+        northEastButton = new JButton("NorthEast");
+        contentPane.add(northEastButton);
+        
+        southWestButton = new JButton("South West");
+        contentPane.add(southWestButton);
+        
+        southEastButton = new JButton("South East");
+        contentPane.add(southEastButton);
+        
+        //contentPane.add(new JButton("Sout West Central Quadrant"));
+        //contentPane.add(new JButton("Central Quadrant"));
+        //contentPane.add(new JButton("East Central Quadrant"));
+        
+        //contentPane.add(new JButton("South West Quadrant"));
+        //contentPane.add(new JButton("South Central Quadrant"));
+        //contentPane.add(new JButton("South East Quadrant"));
 
     }
 
     private static void createAndDisplayGUI() {
         JFrame.setDefaultLookAndFeelDecorated(true);
-
-        JFrame frame = new JFrame("Early Fire Warning System");
+        
+        FirstGui frame = new FirstGui("Early Fire Warning System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Set up the content pane and components in GridLayout
-        addComponentsToPane(frame.getContentPane());
+        frame.createContentPane(frame.getContentPane());
+ 
 
         //Set up frame size & make visible
-        frame.setSize(900, 200);
+        frame.setSize(900, 400);
         frame.setVisible(true);
     }
-
+//main method
     public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+        SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndDisplayGUI();
             }
